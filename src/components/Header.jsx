705e5218ef06599a80657094eb008be3c2661c9a
Joshua Forman <cyborgforman@gmail.com>
2021-04-21 18:01:30 -0600

    initiate router and homepage

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../scss/_header.scss'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="title-container">
+        <h1>Pauly's Pizza</h1>
+        <h2>True Detroit Style Pizza</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header
