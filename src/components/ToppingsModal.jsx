1fab2c11dcaa5cb5e1a88c0bde60a424e7f27d0a
Joshua Forman <cyborgforman@gmail.com>
2021-04-23 15:40:51 -0600

    update toppings checkbox modal

--- src/components/ToppingsModal.jsx
+++ src/components/ToppingsModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, CustomInput } from 'reactstrap';
 
 const ToppingsModal = ({
@@ -8,6 +8,13 @@ const ToppingsModal = ({
   item
 }) => {
 
+  const [toppings, setToppings] = useState([])
+
+  const checkboxHandler = (topping) => {
+    setToppings([...toppings, topping])
+    console.log("toppings", toppings);
+  } 
+
   const {title, description, cost} = item
   return (
     <div>
@@ -16,10 +23,10 @@ const ToppingsModal = ({
         <ModalBody>
           <h3>Anything you would like to add?</h3>
           <div>
-          <CustomInput type="checkbox" id="exampleCustomCheckbox" label="Check this custom checkbox" />
-          <CustomInput type="checkbox" id="exampleCustomCheckbox2" label="Or this one" />
-          <CustomInput type="checkbox" id="exampleCustomCheckbox3" label="But not this disabled one"  />
-          <CustomInput type="checkbox" id="exampleCustomCheckbox4" label="Can't click this label to check!"  />
+          <CustomInput type="checkbox" id="Checkbox1" label="Green Chile" onChange={()=>checkboxHandler("Green Chile")}/>
+          <CustomInput type="checkbox" id="Checkbox2" label="Mushrooms" onChange={()=>checkboxHandler("Mushrooms")}/>
+          <CustomInput type="checkbox" id="Checkbox3" label="Onions" onChange={()=>checkboxHandler("Onions")}/>
+          <CustomInput type="checkbox" id="Checkbox4" label="Spinach" onChange={()=>checkboxHandler("Spinach")}/>
         </div>
         </ModalBody>
         <ModalFooter>
