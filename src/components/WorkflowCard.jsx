3d30943444076a4ac037cd4587fe0b34d80ea341
Joshua Forman <cyborgforman@gmail.com>
2021-04-25 17:49:34 -0600

    update storeapp logic

--- src/components/WorkflowCard.jsx
+++ src/components/WorkflowCard.jsx
@@ -5,11 +5,12 @@ import '../scss/_workflowCard.scss'
 const WorkflowCard = ({ za }) => {
   
   const { pizza, toppings, id } = za
-  
+
   return (
     <div className="workflow-card">
       <h2 className="pizza">{pizza}</h2>
-      <p className="topping">{toppings}</p>
+      {toppings.map((topping)=><p className="topping">{topping}</p>)}
+      
       <button className="advance">+</button>
       <button className="back">-</button>
     </div>
--- src/pages/StoreApp.jsx
+++ src/pages/StoreApp.jsx
@@ -4,44 +4,6 @@ import WorkflowCard from "../components/WorkflowCard";
 import "../scss/_storeApp.scss";
 
 const StoreApp = ({ orderList, setOrderList }) => {
-  const [preparingList, setPreparingList] = useState(orderList);
-  const [bakingList, setBakingList] = useState(orderList);
-  const [perfectingList, setPerfectingList] = useState(orderList);
-  const [servingList, setServingList] = useState(orderList);
-
-  // useEffect(() => {
-  //   return () => {
-  //     if (orderList.length === 1) {
-  //       setPreparingList(orderList[0].filter((el) => el.status === 1));
-  //     } else {
-  //       setPreparingList();
-  //     }
-  //     if (orderList.length === 2) {
-  //       setBakingList(orderList[0].filter((el) => el.status === 2));
-  //     } else {
-  //       setBakingList();
-  //     }
-  //     if (orderList.length === 3) {
-  //       setPerfectingList(orderList[0].filter((el) => el.status === 3));
-  //     } else {
-  //       setPerfectingList();
-  //     }
-  //     if (orderList.length === 4) {
-  //       setServingList(orderList[0].filter((el) => el.status === 4));
-  //     } else {
-  //       setServingList();
-  //     }
-
-  //   };
-  // }, [setOrderList()]);
-
-  console.log(
-    "preparingList",
-    preparingList[0],
-    'bakingList', bakingList, 
-    'orderList', orderList
-    // orderList[0].filter((el) => el.status === 1)
-  );
 
   if (orderList.length === 0) {
     return <h2>No Orders To Make :)</h2>;
@@ -53,28 +15,26 @@ const StoreApp = ({ orderList, setOrderList }) => {
         <div className="phases">
           <div className="preparation">
             <h3>Preparing!</h3>
-            {preparingList &&
-              preparingList.map((order) =>
-                order.map((za) => <WorkflowCard za={za} />)
+            {orderList.map((order) =>
+                order.map((za) => za.status === 1 && <WorkflowCard za={za} />)
               )}
           </div>
           <div className="baking">
             <h3>Baking!</h3>
-            {bakingList &&
-              bakingList.map((order) =>
-                order.map((za) => <WorkflowCard za={za} />)
+            {orderList.map((order) =>
+                order.map((za) => za.status === 2 && <WorkflowCard za={za} />)
               )}
           </div>
           <div className="perfecting">
             <h3>Perfecting!</h3>
-            {perfectingList.map((order) =>
-              order.map((za) => <WorkflowCard za={za} />)
+            {orderList.map((order) =>
+              order.map((za) => za.status === 3 && <WorkflowCard za={za} />)
             )}
           </div>
           <div className="serving">
             <h3>Serving!</h3>
-            {servingList.map((order) =>
-              order.map((za) => <WorkflowCard za={za} />)
+            {orderList.map((order) =>
+              order.map((za) => za.status === 4 && <WorkflowCard za={za} />)
             )}
           </div>
         </div>
