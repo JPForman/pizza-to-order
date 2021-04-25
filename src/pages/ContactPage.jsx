2708ca50acd8ef24464f99acea471ae1d9110dfc
Joshua Forman <cyborgforman@gmail.com>
2021-04-25 14:21:32 -0600

    update content and styles of contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import "../scss/_contactPage.scss";
 
 const ContactPage = () => {
   return (
-    <div>
-      contact me
+    <div className="contact-page-container">
+      <div className="contact">
+        <h1>Contact Us!</h1>
+        <div className="content">
+          <p>Feel free to give us a call!</p>
+          <p>810-555-5555</p>
+          <p>555 Main St.</p>
+          <p>Somewhere, New Mexico</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
